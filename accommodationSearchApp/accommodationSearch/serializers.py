cca65ebf73ae2b506e070d67158a22f0a1954150
Nhất Duy <nhatduy242@gmail.com>
2025-04-19 01:27:13 +0700

    sửa lại việc băm mật khẩu không chính xác, chưa kiểm tra code

--- accommodationSearchApp/accommodationSearch/serializers.py
+++ accommodationSearchApp/accommodationSearch/serializers.py
@@ -31,11 +31,22 @@ class UserSerializer(ModelSerializer):
         return data
     
     def create(self, validated_data):
-       data = validated_data.copy()
-       u = User(**data)
-       u.set_password(u.password)
-       u.save()
-       return u
+        password = validated_data.pop('password')
+        user = User(**validated_data) 
+        user.set_password(password) 
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None) 
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)              
+
+        if password:
+            instance.set_password(password)          
+
+        instance.save()
+        return instance
 
 
 # class AdminSerializer(ItemSerializer):
